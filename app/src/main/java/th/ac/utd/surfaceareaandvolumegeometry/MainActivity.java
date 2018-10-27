ce0f40425127a63da1c1cf713e0a1a2ddf165e9f
FeelingzEXE <jawzainw55@hotmail.com>
2018-10-27 12:33:10 +0700

    27/10/2018 Update
    + add new image button

--- app/src/main/java/th/ac/utd/surfaceareaandvolumegeometry/MainActivity.java
+++ app/src/main/java/th/ac/utd/surfaceareaandvolumegeometry/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private ImageButton button_book;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
                 openActivity2();
             }
         });
+
     }
     public void openActivity2()
     {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
+
+
 }
